a11a468d69a323dc67f65886dde5e13e32615138
Main <natalybon1405@gmail.com>
2023-01-26 10:00:49 +0200

    [HW_24] MongoDb

---
+++ NatalyBondarenko/mongo_db/objects/pie_shop_db.py
@@ -0,0 +1,15 @@
+import pymongo
+
+from Auto_Lessons.HomeWork.NatalyBondarenko.mongo_db.objects.base_db import BaseDb
+
+
+class PieShopDb(BaseDb):
+    def __init__(self):
+        super().__init__("pie", "pie_characteristic")
+
+    def insert_value(self, new_pie_name, new_filling, new_price):
+        """
+            A method to insert value in collection.
+            dict format is {'name': new_name, 'filling': new_filling, 'price': new_price}
+        """
+        return self.collection.insert_one({'name': new_pie_name, 'filling': new_filling, 'price': new_price})
